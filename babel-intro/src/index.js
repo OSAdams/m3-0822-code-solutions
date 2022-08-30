4ad062397cab09122e801df65753f355d396886b
Owen Adams <owenadams.main@gmail.com>
2022-08-30 23:48:32 +0000

    sandbox

--- babel-intro/src/index.js
+++ babel-intro/src/index.js
@@ -42,3 +42,30 @@ const fooBar = () => {
 helloWorld.onCall();
 helloWorld.onSnooze();
 helloWorld.onTendies();
+
+function VeryBig(para1, para2, para3) {
+  this.para1 = para1;
+  this.para2 = para2;
+  this.para3 = para3;
+}
+
+VeryBig.prototype.onCall = function () {
+  console.log(this.para1);
+};
+
+VeryBig.prototype.onSnooze = function () {
+  console.log(this.para2);
+};
+
+VeryBig.prototype.onTendies = function () {
+  console.log(this.para3);
+};
+
+const bazBuz = () => {
+  const tailorMade = new VeryBig('Shifty', 'Sleepy', 'Honey Mussy');
+  tailorMade.onCall();
+  tailorMade.onSnooze();
+  tailorMade.onTendies();
+};
+
+bazBuz();
