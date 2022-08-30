df9b59d1b7182cabbc6a2929b3bf7009ce4cdd8e
Owen Adams <owenadams.main@gmail.com>
2022-08-30 02:51:23 +0000

    updated, ready for review

--- react-elements/src/index.js
+++ react-elements/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
-  React.createElement('h1', null, 'Hello React!'),
-  document.getElementById('root')
-);
+const helloWorld = React.createElement('h1', null, 'Hello React!');
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(helloWorld);
+
+console.log('helloWorld:', helloWorld);
+console.log('container:', container);
+console.log('root:', root);
