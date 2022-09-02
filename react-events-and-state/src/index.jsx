2c1dd7d5a12138e2f4ee3aeb0fdd6c5caa3fe726
Owen Adams <owenadams.main@gmail.com>
2022-09-02 04:40:47 +0000

    WIP

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isClicked: !prevState.isClicked
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={ this.handleClick }>
+        { this.state.isclicked ? 'Thanks!' : 'Click Me!' }
+      </button>
+    );
+  }
+}
+
+const element = (
+  <div>
+    <CustomButton />
+  </div>
+);
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(element);
