31148b43bb5670e8e1930cc8ddc2ec23010fa1c8
Owen Adams <owenadams.main@gmail.com>
2022-09-09 03:36:56 +0000

    create two methods to update the DOM. WIP

--- react-stopwatch-component/src/stopwatch-component.jsx
+++ react-stopwatch-component/src/stopwatch-component.jsx
@@ -4,13 +4,42 @@ export default class StopWatch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      placeholder: true
+      count: 0,
+      isTicking: false
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  handleClick() {
+    if (!this.state.isTicking) {
+      this.interval = setInterval(() => {
+        this.setState({ count: this.state.count + 1 });
+      }, 1000);
+      return this.setState({ isTicking: true });
+    }
+    clearInterval(this.interval);
+    return this.setState({ isTicking: false });
+  }
+
+  handleReset() {
+    return false;
   }
 
   render() {
+    // eslint-disable-next-line
+    console.log(this.state);
     return (
-      <div><p>Hello World</p></div>
+      <>
+      <div className="stopwatch-container">
+        <div className="stopwatch">
+          <p className="stopwatch-count">{ this.state.count }</p>
+        </div>
+        <div className="stopwatch-toggle">
+            <i className={ !this.state.isTicking ? 'fa fa-play' : 'fa fa-pause' } onClick={ this.handleClick } />
+        </div>
+      </div>
+      </>
     );
   }
 }
