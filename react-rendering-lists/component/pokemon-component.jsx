21f213d3beeffabcc47094ef828454e319d199a1
Owen Adams <owenadams.main@gmail.com>
2022-09-13 02:31:35 +0000

    rename pokemon component class

--- react-rendering-lists/component/pokemon-component.jsx
+++ react-rendering-lists/component/pokemon-component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ListGenerator from '../util/list-generator';
 import pokeDex from '../data/pokedex';
 
-export default class PokemonList extends React.Component {
+export default class PokemonComponent extends React.Component {
   render() {
     return <ListGenerator data={ pokeDex } />;
   }
