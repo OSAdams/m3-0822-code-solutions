067cb201643a09dca3a6f9e6f46bd6cc4a798100
Owen Adams <owenadams.main@gmail.com>
2022-09-16 06:04:21 +0000

    complete challenge. ready for review

--- react-accordion-component/components/accordion-component.jsx
+++ react-accordion-component/components/accordion-component.jsx
@@ -12,26 +12,40 @@ export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: this.props.data,
-      display: ''
+      clickContent: ''
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const textContent = event.target.textContent;
+    const { clickContent } = this.state;
+    if (clickContent !== textContent) { this.setState({ clickContent: textContent }); }
+    if (clickContent === textContent) { this.setState({ clickContent: '' }); }
   }
 
   render() {
-    const { data } = this.state;
+    const { clickContent } = this.state;
+    const { data } = this.props;
     const renderData = data.map(index => {
       return (
         <>
           <div className='ac-title' key={ index.number }>
             <h3>{ index.title }</h3>
           </div>
-          <Content content={ index.content } />
+          {
+          clickContent === index.title
+            ? <Content content={index.content} />
+            : clickContent !== index.title
+              ? ''
+              : ''
+          }
         </>
       );
     }
     );
     return (
-      <div className='ac-container'>
+      <div className='ac-container' onClick={ this.handleClick }>
         { renderData }
       </div>
     );
