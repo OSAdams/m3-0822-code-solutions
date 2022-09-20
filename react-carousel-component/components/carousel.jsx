ff666a27d2b0c68afd90e6abd1044ae82377ee15
Owen Adams <owenadams.main@gmail.com>
2022-09-20 01:30:43 +0000

    define methods in carousel component. WIP

--- react-carousel-component/components/carousel.jsx
+++ react-carousel-component/components/carousel.jsx
@@ -15,22 +15,43 @@ export default class Carousel extends React.Component {
     };
     this.previousImage = this.previousImage.bind(this);
     this.nextImage = this.nextImage.bind(this);
+    this.loopImages = this.loopImages.bind(this);
+    this.intervalID = setInterval(this.loopImages, 3000);
   }
 
   previousImage() {
     const { iterator } = this.state;
     const imgArrayLength = this.props.imgData.weekTwo.length;
-    iterator === 0
-      ? this.setState({ iterator: imgArrayLength - 1 })
-      : this.setState({ iterator: iterator - 1 });
+    this.resetInterval();
+    if (iterator === 0) {
+      this.setState({ iterator: imgArrayLength - 1 });
+    } else {
+      this.setState({ iterator: iterator - 1 });
+    }
   }
 
   nextImage() {
     const { iterator } = this.state;
     const imgArrayLength = this.props.imgData.weekTwo.length;
-    iterator === imgArrayLength - 1
-      ? this.setState({ iterator: 0 })
-      : this.setState({ iterator: iterator + 1 });
+    this.resetInterval();
+    if (iterator === imgArrayLength - 1) {
+      this.setState({ iterator: 0 });
+    } else {
+      this.setState({ iterator: iterator + 1 });
+    }
+  }
+
+  loopImages() {
+    const { iterator } = this.state;
+    const imgArrayLength = this.props.imgData.weekTwo.length;
+    iterator < imgArrayLength - 1
+      ? this.setState({ iterator: iterator + 1 })
+      : this.setState({ iterator: 0 });
+  }
+
+  resetInterval() {
+    clearInterval(this.intervalID);
+    this.intervalID = setInterval(this.loopImages, 3000);
   }
 
   render() {
