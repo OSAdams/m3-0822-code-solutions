cd07a0e14f53bc1d120a1d14ae3899a105b51768
Owen Adams <owenadams.main@gmail.com>
2022-09-20 07:24:04 +0000

    complete fetch exercise. ready for review

---
+++ fetch/main.js
@@ -0,0 +1,13 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(data => {
+    console.log('----DATA----');
+    console.log(data);
+  });
+
+fetch('https://pokeapi.co/api/v2/pokemon/1')
+  .then(res => res.json())
+  .then(data => {
+    console.log('---POKEMON---');
+    console.log(data);
+  });
